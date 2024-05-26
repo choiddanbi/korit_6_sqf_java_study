cdcc426d174ce41dfb912491b8339680a932417f
choiddanbi <eksql5955@naver.com>
2024-05-27 06:44:30 +0900

    240827,,

---
+++ src/main/java/com/study/java_study/ch04_제어/Gugudan.java
@@ -0,0 +1,16 @@
+package com.study.java_study.ch04_제어;
+
+public class Gugudan {
+    public static void main(String[] args) {
+        /*
+        [2단]
+        2 x 1 = 2
+        2 x 2 = 4
+        ...
+        2 x 9 = 18
+
+        [9단]
+        ...
+         */
+    }
+}
