f344649ddcea72da1c4c66b1b6a666e885f66594
choiddanbi <eksql5955@naver.com>
2024-06-03 17:23:10 +0900

    240603

--- src/main/java/com/study/java_study/ch15_Static/StudentMain.java
+++ src/main/java/com/study/java_study/ch15_Static/StudentMain.java
@@ -1,9 +1,10 @@
 package com.study.java_study.ch15_Static;
 
 /*
-    싱글톤 디자인 패턴
+    싱글톤 디자인 패턴 >> static 이용!!
     1. 객체를 유일하게 하나만 생성하여 사용하는 기법
     2. 생성자가 private으로 외부에서는 생성할 수 없다.
+      싱글톤 필수 조건 ==> 자신을 담을 인스턴스변수, 외부에서 호출 못하게 생성자 private로, getinstace라는 static 메소드 생성
  */
 
 public class StudentMain {
@@ -11,10 +12,10 @@ public class StudentMain {
         // StudentService studentService = new StudentService();
         // StudentService studentService2 = new StudentService();
         // 원래 이렇게 생성해줬는데 이러면 안됨 > StudentService 생성자를 private 걸었기 때문 > 싱글톤
-        StudentService studentService = StudentService.getInstance(); // 로 생성해줘야함, getInstance() static 객체 생성 > 클래스명.메소드();
-        StudentService studentService2 = StudentService.getInstance(); // 로 생성해줘야함, getInstance()객체
-        // studentService 과 studentService2 느 ㄴ주소까지 아예 동일
-
+        StudentService studentService = StudentService.getInstance(); // 클래스명.메소드(); > static
+        StudentService studentService2 = StudentService.getInstance();
+        // studentService 과 studentService2 는 주소까지 아예 동일 = 객체 생성 없이 했으니까
+        // StudentRepository 에다가 주소 하나 만들어놓구 가져다 쓰는거니까
 
 
         /* 아래와 동일 // 싱글톤일땐 작동 안함
@@ -22,13 +23,12 @@ public class StudentMain {
        studentRepository.add("최단비2");
        StudentRepository.getInstance() = 객체 기능
        */
-        studentService.addStudent(); // 싱글톤이라서 이렇게 씀
-        StudentRepository.getInstance().add("최단비2"); // 원래라면 StudentRe~ studentRe~ = new Su~ > student~.add("최단비2"); > 로 해야함
+        studentService.addStudent(); // 싱글톤이라서 이렇게 씀 > static 변수라서 !!
+        StudentRepository.getInstance().add("최단비2");
         // StudentRepository.getInstance(). 하나의 객체다 = 주소임 // isatance.add
         studentService2.addStudent();
 
 
         studentService.getStudentData();
-
     }
 }
--- src/main/java/com/study/java_study/ch15_Static/StudentService.java
+++ src/main/java/com/study/java_study/ch15_Static/StudentService.java
@@ -2,14 +2,15 @@ package com.study.java_study.ch15_Static;
 
 public class StudentService {
     // 변수 static
-    private static StudentService instance; // 인스턴스 생성
+    private static StudentService instance;
 
     // private 생성자
     private StudentService() {}
 
 
     // 메소드 getInstance()
-    // 싱글톤하기 위한 메소드
+    // 싱글톤하기 위한 메소드!!
+    // 매개변수를 쓰지 않아도 다같이 공유 가능
     public static StudentService getInstance() {
         if(instance == null) {
             instance = new StudentService(); // 새로운 객체 생성
@@ -22,7 +23,7 @@ public class StudentService {
         /* StudentRepository studentRepository = new StudentRepository();
         studentRepository.add("최단비");*/
         StudentRepository.getInstance().add("최단비"); // 주소
-        // StudentRepository.getInstance() => private static StudentService instance ???
+        // StudentRepository.getInstance() => private static StudentService instance 의 주소
     }
 
     // 일반메소드
